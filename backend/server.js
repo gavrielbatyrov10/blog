f60befe068804950fe6c443213c02b0d14c6d9c5
gavriel <gbatyrov10@gmail.com>
2024-11-21 11:37:56 -0500

    initail commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const app = express();
+const PORT = 5000; 
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Simple Node.js and Express App!");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", timestamp: new Date().toISOString() });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
